5ad21010d8f69dbcebb1c7f61bcad40c7f4b54f1
taniska-sketch <taniskasaxena06@gmail.com>
2026-08-10 12:17:42 -0700

    Time: 23 ms (34.63%) | Memory: 29.9 MB (24.42%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans="";
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+          if(s[i]=='*'){
+            st.pop();
+          }
+       
+         else{
+            st.push(s[i]);
+         }
+         }
+         while(!st.empty()){
+         ans.push_back(st.top());
+         st.pop();
+         }
+
+         reverse(ans.begin(),ans.end());
+         return ans;
+
+    }
+};

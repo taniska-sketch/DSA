2a82ee7ff36f085fcb3fce043b21f4fb186a657d
taniska-sketch <taniskasaxena06@gmail.com>
2026-08-11 05:26:04 -0700

    Time: 3 ms (40.75%) | Memory: 12.9 MB (40.49%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+        stack<int> st;
+        unordered_map<int, int> mp;
+
+        // nums2 par Next Greater Element find karo
+        for(int i = nums2.size() - 1; i >= 0; i--) {
+
+            while(!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if(st.empty())
+                mp[nums2[i]] = -1;
+            else
+                mp[nums2[i]] = st.top();
+
+            st.push(nums2[i]);
+        }
+
+        // nums1 ke according answer banao
+        vector<int> res;
+
+        for(int i = 0; i < nums1.size(); i++) {
+            res.push_back(mp[nums1[i]]);
+        }
+
+        return res;
+    }
+};

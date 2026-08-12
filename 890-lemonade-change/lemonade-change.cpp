8ef76e28a42932a0d9cb4f8b97eadfa9a3ac5d20
taniska-sketch <taniskasaxena06@gmail.com>
2026-08-12 13:08:20 -0700

    Time: 0 ms (100.00%) | Memory: 87.3 MB (45.96%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0;
+        int ten=0;
+        for(int i=0;i<bills.size();i++){
+            int money=bills[i];
+            if(money==5){
+                five++;
+            }
+            else if(money==10){
+                if(five==0)
+                return false;
+                else{
+                five--;
+                ten++;
+           }
+
+            }
+            else{
+                if(ten>0){
+                    ten--;
+                
+                if(five==0)
+                return false;
+                five --;}
+            
+            else{
+             if(five<3)
+             return false;
+             five=five-3;}
+        }
+        }
+        return true;
+    }
+};

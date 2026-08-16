c70a873f05d955e10b737c5f61a831854bdfcd0b
taniska-sketch <taniskasaxena06@gmail.com>
2026-08-16 12:25:57 -0700

    Time: 0 ms (100.00%) | Memory: 31.3 MB (82.01%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int guess=low+(high-low)/2;
+            if(target<nums[guess]){
+                  high=guess-1;
+              }
+                else if(target==nums[guess]){
+                    return guess;
+                }
+                else{ low=guess+1;
+                  
+                   
+                }
+                  
+                
+            }
+        return -1;
+    }
+};
